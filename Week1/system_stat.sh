0e53431a71b9d9574de68f3ab61d4003d06fcbde
Balloan <balloan@proton.me>
2023-05-23 15:00:08 -0400

    Improved output

--- Week1/system_stat.sh
+++ Week1/system_stat.sh
@@ -5,6 +5,6 @@ free_mem=$(free -h | awk 'NR==2 {print $NF}')
 #Only gives output from one disk currently
 disk_free=$(df -h --type=ext4 --type=ext3 | awk 'NR==2 {print $4}')
 
-echo "CPU Usage: $(echo "100 - $idle_cpu" | bc)"
+echo "CPU Usage: $(echo "100 - $idle_cpu" | bc)%"
 echo "Free RAM: $free_mem"
 echo "Disk free: $disk_free"
