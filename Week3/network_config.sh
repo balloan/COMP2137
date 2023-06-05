12c88483b5c0f0be53cbdb67d6d772d5b72cb598
Balloan <balloan@proton.me>
2023-06-05 18:08:32 -0400

    Added scripts

---
+++ Week3/network_config.sh
@@ -0,0 +1,12 @@
+#Select second interface; static?
+interface=$(ip -4 -o address show scope global | awk ' NR==2 {print $2}')
+
+static_ip="10.0.10.222"
+netmask="255.255.255.0"
+hostname=$(hostname)
+
+#Network commands
+nmcli connection add con-name $interface type ethernet ifname $interface ipv4.method manual ipv4.address $static_ip $netmask 
+
+#Add to end of /etc/hosts
+echo "$static_ip       $hostname" |sudo tee -a /etc/hosts
---
+++ Week3/network_info.sh
@@ -0,0 +1,11 @@
+#Show LAN interfaces
+echo -e "--------- LAN Interfaces -------------\n"
+ip -4 -o address show scope global |
+awk '{print "Interface:", $2, $4}'
+
+echo -e "\n-------- Network Information --------\n"
+#Hostname associated w/ LAN
+echo "Hostname: $(getent hosts $(hostname -I | cut -d ' ' -f1) | cut -d ' ' -f2)"
+
+echo "Default Gateway: $(ip route show default | cut -d ' ' -f3)"
+echo "Public IP Address: $(curl -s icanhazip.com)"
