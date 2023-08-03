587fabae13c74b9991f9654ca421a56ff9a599a9
balloan <97322695+balloan@users.noreply.github.com>
2023-08-03 12:44:03 -0400

    Modified error handling

--- Assignments/distributed_automation.sh
+++ Assignments/distributed_automation.sh
@@ -81,20 +81,18 @@ exit_on_failure "Restarting rsyslog service on target1"
 
 # Check hostname
 target2_host=$(ssh -o StrictHostKeyChecking=no remoteadmin@target2-mgmt hostname)
-
-if [ "$?" -ne "0" ]; then
-	echo "Unable to SSH into target host; exiting"
-	exit 1
-fi
+exit_on_failure "SSH into target2"
 
 # Change hostname if necessary
 if [ "$target2_host" != "webhost" ]; then
 	echo "Changing server name to webhost"
 	ssh remoteadmin@target2-mgmt hostnamectl "set-hostname webhost"
+ 	exit_on_failure "Changing target2 hostname"
 fi
 
 # Find netplan file to change it
 target2_netplan=$(ssh remoteadmin@target2-mgmt "find /etc/netplan -type f")
+exit_on_failure "Finding /etc/netplan file on target2"
 
 # Exit if multiple netplan files found; unexpected configuration
 if [ $(echo "$target2_netplan" | wc -l) -gt 1 ]; then
@@ -118,38 +116,44 @@ network:
         eth1:
             addresses: [172.16.1.11/24]
 EOF"
+exit_on_failure "Modifying netplan file on target2"
 
 # Apply netplan changes
-ssh remoteadmin@target2-mgmt "netplan apply > /dev/null 2>&1 || { echo 'Netplan failed to apply; exiting'; exit 1; }"
+ssh remoteadmin@target2-mgmt "netplan apply > /dev/null 2>&1"
+exit_on_failure "Applying netplan changes on target2"
+
 
 # Add loghost to /etc/hosts
 ssh remoteadmin@target2-mgmt 'echo "192.168.16.3 loghost" >> /etc/hosts'
+exit_on_failure "Modifying /etc/hosts on target2"
 
 # UFW Settings
-ssh remoteadmin@target2-mgmt "apt-get install ufw -y > /dev/null 2>&1 || { echo 'Failed to install UFW; exiting' ; exit 1; }"
-ssh remoteadmin@target2-mgmt "ufw allow 80/tcp > /dev/null 2>&1 || { echo 'Failed to edit UFW; exiting' ; exit 1; }"
+ssh remoteadmin@target2-mgmt "apt-get install ufw -y > /dev/null 2>&1"
+exit_on_failure "Installing UFW on target2"
+ssh remoteadmin@target2-mgmt "ufw allow 80/tcp > /dev/null 2>&1"
+exit_on_failure "Modifying UFW rules on target2"
 
 # Apache Install
-ssh remoteadmin@target2-mgmt "apt-get install apache2 -y > /dev/null 2>&1 || { echo 'Failed to install Apache; exiting' ; exit 1; }"
+ssh remoteadmin@target2-mgmt "apt-get install apache2 -y > /dev/null 2>&1"
+exit_on_failure "Installing Apache on target2"
 
 # Configure rsyslog on webhost to send logs to loghost
-ssh remoteadmin@target2-mgmt 'echo "*.* @loghost" >> /etc/rsyslog.conf || { echo "Failed to edit syslog conf; exiting" ; exit 1; }'
+ssh remoteadmin@target2-mgmt 'echo "*.* @loghost" >> /etc/rsyslog.conf'
+exit_on_failure "Modifying /etc/rsyslog.conf on target2"
 ssh remoteadmin@target2-mgmt  'systemctl restart rsyslog'
+exit_on_failure "Restarting rsyslog on target2"
 
 # Edit host machine /etc/hosts file
 sudo sed -i 's/192\.168\.16\.10 target1/192.168.16.3 loghost/' /etc/hosts
+exit_on_failure "Modifying /etc/hosts on local machine"
 sudo sed -i 's/192\.168\.16\.11 target2/192.168.16.4 webhost/' /etc/hosts
 
 # Search for the default Apache page; verify that site is up
 wget -qO- http://webhost | grep "It works" > /dev/null
-
-if [ "$?" -ne "0" ]; then
-    echo "Unable to load the webpage from webhost; exiting"
-    exit 1
-fi
+exit_on_failure "Loading page from webhost"
 
 # Check to see how many lines containing webhost are in loghost's syslog
-lines=$(ssh remoteadmin@loghost grep webhost /var/log/syslog | wc -l)
+lines=$(ssh remoteadmin@loghost grep webhost /var/log/syslog | wc -l)"
 
 if [ "$lines" -eq "0" ]; then
 	echo "Unable to retrieve webhost logfiles; exiting"
